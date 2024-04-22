acd8f7ca3b0a3ceadb12d08d99f85d284d7722e6
minhhoang256 <hoang.minhle256@gmail.com>
2024-04-22 16:48:12 +0700

    hoang create recommend for userId

--- src/components/recsForUser/RecForUser.jsx
+++ src/components/recsForUser/RecForUser.jsx
@@ -12,6 +12,7 @@ import {
   SimpleGrid,
   SkeletonText,
   Box,
+  Text
 } from "@chakra-ui/react";
 
 const RecForUser = () => {
@@ -22,77 +23,6 @@ const RecForUser = () => {
   const [num, setNum] = useState(0);
   const [q, setQ] = useState("");
 
-  // const data = {
-  //   user_movie: [
-  //     {
-  //       title: "Seven (a.k.a. Se7en)",
-  //       url: "https://image.tmdb.org/t/p/w500/6yoghtyTpznpBik8EngEmJskVUO.jpg",
-  //     },
-  //     {
-  //       title: "Usual Suspects, The",
-  //       url: "https://image.tmdb.org/t/p/w500/rWbsxdwF9qQzpTPCLmDfVnVqTK1.jpg",
-  //     },
-  //     {
-  //       title: "Bottle Rocket",
-  //       url: "https://image.tmdb.org/t/p/w500/6E7mJ5wpzTFbYFbJvTzzZ8Til9C.jpg",
-  //     },
-  //     {
-  //       title: "Rob Roy",
-  //       url: "https://image.tmdb.org/t/p/w500/4QuypgbNYmPECAAHnV3QY8qEpOz.jpg",
-  //     },
-  //     {
-  //       title: "Canadian Bacon",
-  //       url: "https://image.tmdb.org/t/p/w500/bzK2aQ8xUB67kwCR5LHTSY99HtT.jpg",
-  //     },
-  //     {
-  //       title: "Desperado",
-  //       url: "https://image.tmdb.org/t/p/w500/e3gwpBeXpvGZsxUya9zNym5QXrw.jpg",
-  //     },
-  //     {
-  //       title: "Billy Madison",
-  //       url: "https://image.tmdb.org/t/p/w500/iwk9pWR6MwTInEQc8Vw5vGHjeQ0.jpg",
-  //     },
-  //     {
-  //       title: "Dumb & Dumber (Dumb and Dumber)",
-  //       url: "https://image.tmdb.org/t/p/w500/4LdpBXiCyGKkR8FGHgjKlphrfUc.jpg",
-  //     },
-  //     {
-  //       title: "Star Wars: Episode IV - A New Hope",
-  //       url: "https://image.tmdb.org/t/p/w500/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg",
-  //     },
-  //     {
-  //       title: "Tommy Boy",
-  //       url: "https://image.tmdb.org/t/p/w500/6m1xJqfViDkmNmKUKvTSJ5fo0k4.jpg",
-  //     },
-  //   ],
-  //   recs_movie: [
-  //     {
-  //       title: "Four Rooms",
-  //       score: 0.9866636,
-  //       url: "https://image.tmdb.org/t/p/w500/75aHn1NOYXh4M7L5shoeQ6NGykP.jpg",
-  //     },
-  //     {
-  //       title: "Crippled Avengers (Can que) (Return of the 5 Deadly Venoms)",
-  //       score: 0.9861176,
-  //       url: "https://image.tmdb.org/t/p/w500/s6snsNvb1UBKIqfvoKpB1p6D2NM.jpg",
-  //     },
-  //     {
-  //       title: "Nine to Five (a.k.a. 9 to 5)",
-  //       score: 0.9687842,
-  //       url: "https://image.tmdb.org/t/p/w500/34cDJ8DMVb0RuIg5zDuotCGdNKb.jpg",
-  //     },
-  //     {
-  //       title: "Fast Five (Fast and the Furious 5, The)",
-  //       score: 0.9675164,
-  //       url: "https://image.tmdb.org/t/p/w500/gEfQjjQwY7fh5bI4GlG0RrBu7Pz.jpg",
-  //     },
-  //     {
-  //       title: "Father of the Bride Part II",
-  //       score: 0.9645666,
-  //       url: "https://image.tmdb.org/t/p/w500/rj4LBtwQ0uGrpBnCELr716Qo3mw.jpg",
-  //     },
-  //   ],
-  // };
 
   const handleRecommend = async () => {
     if (userId > 0 && numLast > 0 && num > 0) {
@@ -143,12 +73,14 @@ const RecForUser = () => {
               placeholder="Number of movies the user last rated highly"
               onChange={(e) => setNumLast(e.target.value)}
               marginTop={4}
+            
             />
           </div>
           <div style={{ width: "45%" }}>
             <Input
               placeholder="Number of movies recommend"
               onChange={(e) => setNum(e.target.value)}
+
             />
             <Input
               placeholder="...q"
